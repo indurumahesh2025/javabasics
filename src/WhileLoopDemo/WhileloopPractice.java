dfa3bc2719256c4b1f59f08bb3017b47b4e04184
indurumahesh2025 <indurumahesh2025@gmail.com>
2025-02-25 21:00:51 +0530

    Added Whileloop demo

--- src/WhileLoopDemo/WhileloopPractice.java
+++ src/WhileLoopDemo/WhileloopPractice.java
@@ -24,9 +24,13 @@ public class WhileloopPractice {
     public static void printCategories(String str){
         int i = 0;
         while (true){
+            // Find the index of the first occurrence of the substring "category:" in the string str starting from index i
             int found = str.indexOf("category:", i);
+            // If the substring "category:" is not found in the string str starting from index i, then break out of the loop
             if (found == -1) break;
+            // The start index of the category name is 9 characters after the index of the substring "category:"
             int start = found + 9;
+            // The end index of the category name is the index of the first space character after the start index
             int end = str.indexOf(" ", start);
             System.out.println(str.substring(start, end));
             i = end + 1;
